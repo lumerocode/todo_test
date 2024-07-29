6360b69d774abf0fd5ecd5bd39018d414c82ac60
lumerocode <luismelendez0894@gmail.com>
2024-07-29 17:23:38 -0500

    feat: update composable useTodos

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
 
-createApp(App).mount('#app')
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app');
