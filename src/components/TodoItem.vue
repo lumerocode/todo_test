3f3bdaeb018118de7a392988e70c59d4037cc366
lumerocode <luismelendez0894@gmail.com>
2024-07-30 02:31:53 -0500

    feat: update mobile layout

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="todo-item">
-    <div class="todo-item__left">
-      <div class="btn-check">
-        <font-awesome-icon icon="check"/>
-      </div>
-      <span :class="[todo.status, { 'done': todo.status === 'DONE' }]">{{ todo.text }}</span>
+    <div class="btn-check">
+      <font-awesome-icon icon="check"/>
     </div>
-    <div class="todo-item__right"> 
-      <select v-model="todo.status">
-        <option value="PENDING">Pending</option>
-        <option value="IN_PROGRESS">In Progress</option>
-        <option value="DONE">Done</option>
-      </select>
-      <div class="bnt-list btn-edit">
-        <button @click="editTodo">
-          <font-awesome-icon icon="edit"/>
-        </button>
+    <div class="todo-item-content">
+      <div class="todo-item__left">
+        <span :class="[todo.status, { 'done': todo.status === 'DONE' }]">{{ todo.text }}</span>
       </div>
-      <div class="btn-list btn-trash">
-        <button @click="deleteTodo">
-          <font-awesome-icon icon="trash"/>
-        </button>
+      <div class="todo-item__right"> 
+        <select v-model="todo.status">
+          <option value="PENDING">Pending</option>
+          <option value="IN_PROGRESS">In Progress</option>
+          <option value="DONE">Done</option>
+        </select>
+        <div class="bnt-list btn-edit">
+          <button @click="editTodo">
+            <font-awesome-icon icon="edit"/>
+          </button>
+        </div>
+        <div class="btn-list btn-trash">
+          <button @click="deleteTodo">
+            <font-awesome-icon icon="trash"/>
+          </button>
+        </div>
       </div>
     </div>
   </div>
